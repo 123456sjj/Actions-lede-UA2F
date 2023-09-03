fdc7343f7259a32e2e10dd4bd6d986ba3ea458b9
123456sjj <79307674+123456sjj@users.noreply.github.com>
2023-09-03 21:24:44 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,5 +1,5 @@
-target=$(grep "^CONFIG_TARGET" .config --max-count=1 | awk -F "=" '{print $1}' | awk -F "_" '{print $3}')
-for configFile in $(ls target/linux/$target/config*)
-do
-    echo -e "\nCONFIG_NETFILTER_NETLINK_GLUE_CT=y" >> $configFile
-done
+# target=$(grep "^CONFIG_TARGET" .config --max-count=1 | awk -F "=" '{print $1}' | awk -F "_" '{print $3}')
+# for configFile in $(ls target/linux/$target/config*)
+# do
+#    echo -e "\nCONFIG_NETFILTER_NETLINK_GLUE_CT=y" >> $configFile
+# done
